e78e06ed299cd7e3ec6be22a20e5b9658b46f449
gjmb <gjmbarbosa@gmail.com>
2023-10-14 11:54:47 -0300

    creates components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameCardComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
